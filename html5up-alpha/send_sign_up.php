963889dba9006b89c5150ea308ab714cfcc41a5f
2015gguan <grantg2008@gmail.com>
2018-02-19 18:42:35 -0500

    small changes to sending php

--- html5up-alpha/send_sign_up.php
+++ html5up-alpha/send_sign_up.php
@@ -1,10 +1,7 @@
-
 <?php
 //Connecting to sql db.
 $connect = mysqli_connect("localhost","root","","customerInfo");
 //Sending form data to sql db.
 mysqli_query($connect,"INSERT INTO custInfo (Name, Email, Address, City, State, Zip, Password)
-VALUES ('$_SIGNUP[Name]', '$_SIGNUP[Email]', '$_SIGNUP[Address]', '$_SIGNUP[City]', '$_SIGNUP[State]',, '$_SIGNUP[Zip]', '$_SIGNUP[Password]')";
-
-VALUES ('$_POST[post_cat<?php')
+VALUES ('$_POST[Name]', '$_POST[Email]', '$_POST[Address]', '$_POST[City]', '$_POST[State]',, '$_POST[Zip]', '$_POST[Password]')";
 ?>
