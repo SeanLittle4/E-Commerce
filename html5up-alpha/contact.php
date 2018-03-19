f45021ac343ee7bf6578291027d96814f7f28d21
2015gguan <grantg2008@gmail.com>
2018-03-19 16:03:23 -0400

    contact.php

---
+++ html5up-alpha/contact.php
@@ -0,0 +1,166 @@
+<!DOCTYPE HTML>
+<!--
+	Alpha by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+
+<html>
+	<?php
+	//use PHPMailer\PHPMailer\PHPMailer;
+//use PHPMailer\PHPMailer\Exception;
+	require("/vendor/PHPMailer/PHPMailer/src/PHPMailer.php");
+	require("/vendor/PHPMailer/PHPMailer/src/SMTP.php");
+	require("/vendor/PHPMailer/PHPMailer/src/Exception.php");
+	require 'vendor/autoload.php';
+    //Create a new PHPMailer instance
+    $mail = new PHPMailer\PHPMailer\PHPMailer(true);
+//echo getcwd();
+	$msg = '';
+//Don't run this unless we're handling a form submission
+try{
+if (array_key_exists('email', $_POST)) {
+    date_default_timezone_set('Etc/UTC');
+    //require '../PHPMailerAutoload.php';
+    
+	//CUT HERE
+
+    //Tell PHPMailer to use SMTP - requires a local mail server
+    //Faster and safer than using mail()
+    $mail->isSMTP();
+    //$mail->Host = 'localhost';
+    //$mail->Port = 25;
+    //https://stackoverflow.com/questions/28906487/fatal-e
+    $mail->SMTPDebug = 2;
+    $mail->SMTPauth = true;
+    $mail->SMTPSecure = 'tls';
+    $mail->Host = 'smtp.gmail.com';
+    $mail->Port = 587;
+    $mail->Username = "pantheon.funding";
+    $mail->Password = "ecommercepantheon";
+    //Use a fixed address in your own domain as the from address
+    //**DO NOT** use the submitter's address here as it will be forgery
+    //and will cause your messages to fail SPF checks
+    $mail->setFrom('pantheon.funding@gmail.com');
+    //Send the message to yourself, or whoever should receive contact for submissions
+    $mail->addAddress('pantheon.funding@gmail.com');
+    //Put the submitter's address in a reply-to header
+    //This will fail if the address provided is invalid,
+    //in which case we should ignore the whole request
+    if ($mail->addReplyTo($_POST['email'], $_POST['name'])) {
+        $mail->Subject = $_POST['subject'];
+        //Keep it simple - don't use HTML
+        $mail->isHTML(false);
+        //Build a simple message body
+        $mail->Body = <<<EOT
+		Email: {$_POST['email']}
+		Name: {$_POST['name']}
+		Message: {$_POST['message']}
+EOT;
+        //Send the message, check for errors
+	        if (!$mail->send()) {
+	            //The reason for failing to send will be in $mail->ErrorInfo
+	            //but you shouldn't display errors to users - process the error, log it on your server.
+	            $msg = 'Sorry, something went wrong. Please try again later.';
+	        } else {
+	            $msg = 'Message sent! Thanks for contacting us.';
+	        }
+	    } else {
+	        $msg = 'Invalid email address, message ignored.';
+	    }
+	}
+} catch (Exception $e) {
+    echo 'Message could not be sent. Mailer Error: ', $mail->ErrorInfo;
+}
+
+?>
+	<head>
+		<title>Contact Us</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+	</head>
+	<body>
+		<div id="page-wrapper">
+
+			<!-- Header -->
+				<header id="header">
+					<h1><a href="index.html"><img src="images/pantheon.png" style = "height: 100%" ></a></h1>
+					<nav id="nav">
+						<ul>
+							<li><a href="index.html">Home</a></li>
+							<li><a href="contact.php">Contact</a></li>
+							<li><a href="about_us.html">About Us</a></li> 
+							<li><a href="SignUp.php" class="button">Sign Up</a>
+							<li><a href="login.html" class="button">Log In</a></li>
+						</ul>
+					</nav>
+				</header>
+
+			<!-- Main -->
+				<section id="main" class="container 75%">
+					<header>
+						<h2>Contact Us</h2>
+						<p>Tell us what you think about our little operation.</p>
+					</header>
+					<div class="box">
+						<form method="post" action="#">
+							<div class="row uniform 50%">
+								<div class="6u 12u(mobilep)">
+									<input type="text" name="name" id="name" value="" placeholder="Name" />
+								</div>
+								<div class="6u 12u(mobilep)">
+									<input type="email" name="email" id="email" value="" placeholder="Email" />
+								</div>
+							</div>
+							<div class="row uniform 50%">
+								<div class="12u">
+									<input type="text" name="subject" id="subject" value="" placeholder="Subject" />
+								</div>
+							</div>
+							<div class="row uniform 50%">
+								<div class="12u">
+									<textarea name="message" id="message" placeholder="Enter your message" rows="6"></textarea>
+								</div>
+							</div>
+							<div class="row uniform">
+								<div class="12u">
+									<ul class="actions align-center">
+										<li><input type="submit" value="Send Message" /></li>
+									</ul>
+								</div>
+							</div>
+						</form>
+					</div>
+				</section>
+
+			<!-- Footer -->
+				<footer id="footer">
+					<ul class="icons">
+						<li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
+						<li><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
+						<li><a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
+						<li><a href="#" class="icon fa-github"><span class="label">Github</span></a></li>
+						<li><a href="#" class="icon fa-dribbble"><span class="label">Dribbble</span></a></li>
+						<li><a href="#" class="icon fa-google-plus"><span class="label">Google+</span></a></li>
+					</ul>
+					<ul class="copyright">
+						<li>&copy; Untitled. All rights reserved.</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+					</ul>
+				</footer>
+
+		</div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.dropotron.min.js"></script>
+			<script src="assets/js/jquery.scrollgress.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
