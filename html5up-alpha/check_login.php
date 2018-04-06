fc6acd13f73e3d7f340f327a36b866b90795a70e
Sean Little <seanlittle@Seans-MBP.hsd1.va.comcast.net>
2018-04-06 13:26:38 -0400

    login error message update

--- html5up-alpha/check_login.php
+++ html5up-alpha/check_login.php
@@ -10,11 +10,17 @@ $result = mysqli_query($connect, $sql);
 if(mysqli_num_rows($result) != 0) {
 	$row = $result->fetch_assoc();
     if($secret == $row["Password"]) {
+
     	//Not quite sure if I'm doing this part right. Trying to start a session which stores a cookie on the user's computer.
     	session_start();
     	$_SESSION['login_user'] = $row["Name"];
     	echo $_SESSION['login_user'];
-}		
+	} else {
+		$message = "Your email or password is incorrect";
+		echo "<script type = 'text/javascript'> alert('$message');
+		window.location.href='SignUp.php';
+		</script>";
+	}		
 
 } else {
 	$message = "Your email or password is incorrect";
